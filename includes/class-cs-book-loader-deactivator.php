d470e2e93d0935ffa30f70ee93bf7c0a33ba50d1
h-lakkad1998 <hardiklakkad2@gmail.com>
2025-09-16 11:46:36 +0530

    remove cache on deactivation added

--- includes/class-cs-book-loader-deactivator.php
+++ includes/class-cs-book-loader-deactivator.php
@@ -30,6 +30,16 @@ class Cs_Book_Loader_Deactivator {
 	 * @since    1.0.0
 	 */
 	public static function deactivate() {
+		global $wpdb;
+		$like1 = $wpdb->esc_like( '_transient_cs_books_' ) . '%';
+		$like2 = $wpdb->esc_like( '_transient_timeout_cs_books_' ) . '%';
+
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+		$count = (int) $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->options} WHERE option_name LIKE %s OR option_name LIKE %s", $like1, $like2 ) );
+
+		$count = ( $count >= 2 ) ? $count / 2 : 0; 
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+		$wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->options} WHERE option_name LIKE %s OR option_name LIKE %s", $like1, $like2 ) );
 
 	}
 
